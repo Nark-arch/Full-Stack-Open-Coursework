6c22db629d60eb179c84f36d9eefa5c6bec15007
ksn.ark <69609693+ksn-ark@users.noreply.github.com>
2023-10-05 15:09:58 +0300

    1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,7 +1,7 @@
 const Part = (props) => {
   return (
     <p>
-      {props.p} {props.e}
+      {props.p.name} {props.p.exercises}
     </p>
   )
 }
@@ -17,33 +17,38 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part p={props.p1} e={props.e1} />
-      <Part p={props.p2} e={props.e2} />
-      <Part p={props.p3} e={props.e3} />
+      <Part p={props.p1} />
+      <Part p={props.p2} />
+      <Part p={props.p3} />
     </>
   )
 }
 
 const Total = (props) => {
   return (
-    <p> Number of exercises {props.e1+props.e2+props.e3} </p>
+    <p> Number of exercises {props.e1.exercises+props.e2.exercises+props.e3.exercises} </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3=14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header c={course} />
-      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3}/>
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Content p1={part1} p2={part2} p3={part3} />
+      <Total e1={part1} e2={part2} e3={part3} />
     </div>
   )
 }
