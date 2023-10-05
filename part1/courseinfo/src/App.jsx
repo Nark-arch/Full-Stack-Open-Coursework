50ff6fdd0d12a85cd2a64ae94ac2ecccebfaf8b8
ksn.ark <69609693+ksn-ark@users.noreply.github.com>
2023-10-05 15:19:51 +0300

    Exc 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,38 +17,40 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part p={props.p1} />
-      <Part p={props.p2} />
-      <Part p={props.p3} />
+      <Part p={props.p[0]} />
+      <Part p={props.p[1]} />
+      <Part p={props.p[2]} />
     </>
   )
 }
 
 const Total = (props) => {
   return (
-    <p> Number of exercises {props.e1.exercises+props.e2.exercises+props.e3.exercises} </p>
+    <p> Number of exercises {props.p[0].exercises+props.p[1].exercises+props.p[2].exercises} </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header c={course} />
-      <Content p1={part1} p2={part2} p3={part3} />
-      <Total e1={part1} e2={part2} e3={part3} />
+      <Content p={parts} />
+      <Total p={parts} />
     </div>
   )
 }
