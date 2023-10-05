4d418a12f4549391ed876246a50b7ee5ed3ee99c
ksn.ark <69609693+ksn-ark@users.noreply.github.com>
2023-10-05 15:23:35 +0300

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,16 +3,16 @@ const Part = (props) => {
     <p>
       {props.p.name} {props.p.exercises}
     </p>
-  )
-}
+  );
+};
 
 const Header = (props) => {
   return (
-      <>
+    <>
       <h1>{props.c}</h1>
-      </>
-  )
-}
+    </>
+  );
+};
 
 const Content = (props) => {
   return (
@@ -21,38 +21,43 @@ const Content = (props) => {
       <Part p={props.p[1]} />
       <Part p={props.p[2]} />
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
-    <p> Number of exercises {props.p[0].exercises+props.p[1].exercises+props.p[2].exercises} </p>
-  )
-}
+    <p>
+      Number of exercises{" "}
+      {props.p[0].exercises + props.p[1].exercises + props.p[2].exercises}{" "}
+    </p>
+  );
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header c={course} />
-      <Content p={parts} />
-      <Total p={parts} />
+      <Header c={course.name} />
+      <Content p={course.parts} />
+      <Total p={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
