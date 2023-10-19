120cfd35609eba7c93b841f653176484c3041d74
ksn.ark <69609693+ksn-ark@users.noreply.github.com>
2023-10-19 06:12:46 +0300

    1.11 step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,12 +4,50 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
+const StatisticsLine = (props) => {
+  return (
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+  );
+};
+
+const Statistics = (props) => {
+  //statistical calculations
+  const all = props.good + props.neutral + props.bad;
+
+  const average = (props.good * 1 + props.neutral * 0 + props.bad * -1) / all;
+
+  const positivePercentage = (props.good / all) * 100;
+
+  //if null input display
+  if (all === 0) {
+    return <p>No Feedback Given</p>;
+  }
+
+  //non null display
+  return (
+    <table>
+      <tbody>
+      <StatisticsLine text="good" value={props.good} />
+      <StatisticsLine text="neutral" value={props.neutral} />
+      <StatisticsLine text="bad" value={props.bad} />
+      <StatisticsLine text="all" value={all} />
+      <StatisticsLine text="average" value={average} />
+      <StatisticsLine text="percentage" value={positivePercentage} />
+      </tbody>
+    </table>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  // click handlers for all three buttons
   const handleGoodClick = () => {
     const newGood = good + 1;
     setGood(newGood);
@@ -25,14 +63,8 @@ const App = () => {
     setBad(newBad);
   };
 
-  const all = good + neutral + bad
-
-  const average = (good * 1 + neutral * 0 + bad *-1) / all
-
-  const positivePercentage = (good / all) *100
-
   return (
-    <div>
+    <>
       <h1>Give Feedback</h1>
       <li>
         <Button text="good" handleClick={handleGoodClick} />
@@ -40,13 +72,8 @@ const App = () => {
         <Button text="bad" handleClick={handleBadClick} />
       </li>
       <h1>Statistics</h1>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {all} <br />
-      average {average} <br />
-      positive {positivePercentage}% <br />
-    </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
   );
 };
 
