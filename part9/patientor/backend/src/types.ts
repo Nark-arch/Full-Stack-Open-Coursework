7a9973b6dbdb083be2b9269d5d084dc21822d980
Nark <sooryarox2004@gmail.com>
2024-08-06 16:37:47 +0300

    9.22

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -10,8 +10,42 @@ export type Diagnosis = {
   latin?: string;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entries {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: { date: string; criteria: string };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type Patient = {
   id: string;
@@ -20,7 +54,7 @@ export type Patient = {
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries: Entries[];
+  entries: Entry[];
 };
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
